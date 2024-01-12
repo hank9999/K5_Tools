67b10c3b28824eeae8c3b7071cde34daa382649d
hank9999 <hank9999@qq.com>
2024-01-13 02:59:06 +0800

    feat: relayout the gui, add two button

--- main.py
+++ main.py
@@ -24,44 +24,65 @@ class TextRedirector(tk.Text):
 
 def main():
     window.title(f'K5/K6 小工具集 v{version}')
+    window.geometry('420x340')
     label1 = tk.Label(window, text=f'K5/K6 小工具集 v{version} (BG4IST - hank9999)')
-    label1.grid(row=0, column=0, columnspan=26, padx=10, pady=(10, 0), sticky='w')
+    label1.place(x=10, y=10)
 
     label2 = tk.Label(window, text='当前操作: 无')
-    label2.grid(row=1, column=0, columnspan=26, padx=10, pady=(0, 10), sticky='w')
+    label2.place(x=10, y=30)
 
     serial_port_label = tk.Label(window, text='串口')
-    serial_port_label.grid(row=2, column=0, padx=(10, 0), pady=10, sticky='w')
+    serial_port_label.place(x=10, y=60)
     serial_port_combo = ttk.Combobox(window, values=[], width=8)
     serial_port_combo['postcommand'] = lambda: serial_port_combo_postcommand(serial_port_combo)
     serial_port_combo.bind(
         '<<ComboboxSelected>>',
         lambda event: serial_port_combo_callback(event, serial_port_combo.get(), label2)
     )
-    serial_port_combo.grid(row=2, column=1, padx=(0, 10), pady=10, sticky='w')
+    serial_port_combo.place(x=40, y=60)
+
+    serial_port_label = tk.Label(window, text='EEPROM')
+    serial_port_label.place(x=125, y=60)
+    serial_port_combo = ttk.Combobox(window, values=[], width=8)
+    serial_port_combo['postcommand'] = lambda: serial_port_combo_postcommand(serial_port_combo)
+    serial_port_combo.bind(
+        '<<ComboboxSelected>>',
+        lambda event: serial_port_combo_callback(event, serial_port_combo.get(), label2)
+    )
+    serial_port_combo.place(x=185, y=60)
+
+    serial_port_label = tk.Label(window, text='固件版本')
+    serial_port_label.place(x=270, y=60)
+    serial_port_combo = ttk.Combobox(window, values=[], width=8)
+    serial_port_combo['postcommand'] = lambda: serial_port_combo_postcommand(serial_port_combo)
+    serial_port_combo.bind(
+        '<<ComboboxSelected>>',
+        lambda event: serial_port_combo_callback(event, serial_port_combo.get(), label2)
+    )
+    serial_port_combo.place(x=325, y=60)
 
     clean_eeprom_button = tk.Button(
         window,
         text='清空EEPROM',
         command=lambda: clean_eeprom(serial_port_combo.get(), window, progress, label2)
     )
-    clean_eeprom_button.grid(row=3, column=0, columnspan=2, padx=(10, 60), pady=10, sticky='w')
+    clean_eeprom_button.place(x=10, y=100)
 
     write_font_button = tk.Button(
         window,
         text='写入字库',
         command=lambda: write_font(serial_port_combo.get(), window, progress, label2)
     )
-    write_font_button.grid(row=3, column=1, padx=(60, 10), pady=10, sticky='w')
+    write_font_button.place(x=105, y=100)
 
-    textbox = tk.Text(window, width=40, height=10)
-    textbox.grid(row=4, column=0, columnspan=26, padx=10, pady=10, sticky='w')
+    textbox = tk.Text(window, width=56, height=10)
+    textbox.place(x=10, y=145)
     sys.stdout = TextRedirector(textbox)
 
     # 创建进度条
-    progress = ttk.Progressbar(window, orient='horizontal', length=260, mode='determinate')
+    progress = ttk.Progressbar(window, orient='horizontal', length=397, mode='determinate')
     # 放置进度条在窗口底部
-    progress.grid(row=5, column=0, columnspan=26, padx=10, pady=10, sticky='ew')
+    progress.place(x=10, y=300)
 
     log('K5/K6 小工具集 BG4IST - hank9999\n')
 
