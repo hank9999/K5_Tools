5485b66dfc99810d08c5f4815d2df2385568655c
hank9999 <hank9999@qq.com>
2024-02-04 20:02:04 +0800

    feat: set tip wait time to 100ms

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ style = ttk.Style(config.get('Settings', 'theme'))
 
 class Tooltip(object):
     def __init__(self, widget, text='widget info'):
-        self.waittime = 500  # miliseconds
+        self.waittime = 100  # miliseconds
         self.wraplength = 180  # pixels
         self.widget = widget
         self.text = text
